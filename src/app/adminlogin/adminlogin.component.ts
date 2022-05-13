1f748bc5f7e0aee67ab68ac3b904123d4677a6d7
giridhar-vakkanthula <girichowdary146@gmail.com>
2022-05-13 16:24:53 +0530

    admin

--- src/app/adminlogin/adminlogin.component.ts
+++ src/app/adminlogin/adminlogin.component.ts
@@ -21,13 +21,7 @@ password: string | undefined;
   login() : void {
     if(this.username == 'admin' && this.password == 'admin'){
      this.router.navigate(["organization"]);
-    }else if(this.username == 'admin1' && this.password == 'admin1'){
-      this.router.navigate(["organization"]);
-    }else if(this.username == 'admin2' && this.password == 'admin2'){
-      this.router.navigate(["organization"]);
-    }else if(this.username == 'admin3' && this.password == 'admin3'){
-      this.router.navigate(["organization"]);
-    }  
+    }
     else {
       alert("Invalid credentials");
     }
